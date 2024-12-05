2d1c92439a5b95d2fe18a906addc0253643adbfa
pvictorca <pvictor_ca@protonmail.com>
2024-12-05 18:03:52 -0300

    Quartz sync: Dec 5, 2024, 6:03 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,36 +1,13 @@
+// @ts-ignore
+import clipboardScript from "./scripts/clipboard.inline"
+import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import "./styles/custom.scss"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return (
-    <div id="quartz-body">
-      <div className="custom-layout">
-        {/* Left Sidebar */}
-        <aside className="custom-sidebar-left">
-          <div className="custom-sidebar-left-inner">
-            <div className="custom-site-title">Seu Site</div>
-            <nav className="custom-nav">
-              {/* Navigation virá aqui depois */}
-            </nav>
-          </div>
-        </aside>
-
-        {/* Main Content */}
-        <main className="custom-main">
-          <div className="custom-content">
-            {children}
-          </div>
-        </main>
-
-        {/* Right Sidebar */}
-        <aside className="custom-sidebar-right">
-          <div className="custom-sidebar-right-inner">
-            {/* Graph/Outline virão aqui depois */}
-          </div>
-        </aside>
-      </div>
-    </div>
-  )
+  return <div id="quartz-body">{children}</div>
 }
+
+Body.afterDOMLoaded = clipboardScript
+Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
