1bb3d531912ab2a625e9c7c5064e75c22e1a6720
pvictorca <pvictor_ca@protonmail.com>
2024-12-05 17:59:59 -0300

    Quartz sync: Dec 5, 2024, 5:59 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,10 +1,38 @@
-// @ts-ignore
+// Body.tsx
 import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div id="quartz-body">
+      <div className="custom-layout">
+        {/* Left Sidebar */}
+        <aside className="custom-sidebar-left">
+          <div className="custom-sidebar-left-inner">
+            <div className="custom-site-title">Seu Site</div>
+            <nav className="custom-nav">
+              {/* Navigation virá aqui */}
+            </nav>
+          </div>
+        </aside>
+
+        {/* Main Content */}
+        <main className="custom-main">
+          <div className="custom-content">
+            {children}
+          </div>
+        </main>
+
+        {/* Right Sidebar */}
+        <aside className="custom-sidebar-right">
+          <div className="custom-sidebar-right-inner">
+            {/* Graph/Outline virão aqui */}
+          </div>
+        </aside>
+      </div>
+    </div>
+  )
 }
 
 Body.afterDOMLoaded = clipboardScript
